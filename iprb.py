c833ec6ba4f46ae4d036bbe45310a1618adfb39b
BeljajevM <maxim.beljajev@dlubal.com>
2020-03-06 06:40:53 +0100

    One problem has been solved.

---
+++ iprb.py
@@ -0,0 +1,7 @@
+def IPRB(homo_dominant, hetero, homo_recessive):
+    """Mendel's First Law"""
+    organisms = homo_dominant + homo_recessive + hetero
+    
+
+if __name__ == "__main__":
+    print(IPRB(2, 2, 2))
